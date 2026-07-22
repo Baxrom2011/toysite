527c76264f098aeb33508dee668164f1cb843003
Baxrom <151901299+Baxrom2011@users.noreply.github.com>
2026-07-22 18:22:37 +0500

    Remove commented translations from debts.js

--- backend/routes/debts.js
+++ backend/routes/debts.js
@@ -9,22 +9,11 @@ const {
     restoreDebt
 } = require('../controllers/debtController');
 
-// Barcha qarzlarni olish
 router.get('/', getDebts);
-
-// Qarz statistikasi
 router.get('/stats', getDebtStats);
-
-// Mijozning qarzlarini olish
 router.get('/customer/:customerId', getCustomerDebts);
-
-// Qarzni to'lash
 router.post('/:id/pay', payDebt);
-
-// Qarzni qayta tiklash
 router.post('/:id/restore', restoreDebt);
-
-// Qarzni o'chirish
 router.delete('/:id', deleteDebt);
 
 module.exports = router;
